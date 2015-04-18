6d5ef60118e12ed5cf812447dd9d400fe647b5a8
DenisKalfon <kalfondenis@gmail.com>
2015-04-18 16:22:26 +0200

    Commit Create site

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Denis KALFON - Intégrateur Web Lyon</title>
+        <link rel="stylesheet" type="text/css" media="screen" href="/DK-Site/css/bootstrap.min.css"  />
+        <link rel="stylesheet" type="text/css" media="screen" href="/DK-Site/css/font-awesome.min.css"  />
+        <link rel="stylesheet" type="text/css" media="screen" href="/DK-Site/css/style.css"  />
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-default">
+                <div class="container container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <h1>Denis KALFON</h1>
+                        <h2>Intégrateur WEB</h2>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a class="scrollTo" href="#">&Agrave; PROPOS</a></li>
+                            <li><a class="scrollTo" href="#">PORTFOLIO</a></li>
+                            <li><a class="scrollTo" href="#">CONTACT</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <?php
+        // put your code here
+        ?>
+        <section id="section-apropos">
+            <div class="container">
+                <h3>&Agrave; PROPOS</h3>
+            </div>
+        </section>
+        <section id="section-portfolio">
+            <div class="container">
+                <h3>PORTFOLIO</h3>
+            </div>
+        </section>
+        <section id="section-contact">
+            <div class="container">
+                <h3>CONTACT</h3>
+            </div>
+        </section>
+        <aside class="to-top">
+            toto
+        </aside>
+        <footer>
+            <div class="container">
+                
+            </div>
+        </footer>
+        <script src="/DK-Site/js/jquery-1.11.2.min.js"></script>
+        <script src="/DK-Site/js/bootstrap.min.js"></script>
+        <script src="/DK-Site/js/scripts.js"></script>
+    </body>
+</html>
---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+    $('.scrollTo').click( function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        return false;
+    });
+    $(window).scroll(function (){ 
+        $(".to-top").fadeIn();
+    });
+});
+
